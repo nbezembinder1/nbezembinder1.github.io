3c4b4247f3a89f5bdb255c9ead302558ef81572b
Nick Bezembinder <nick.bezembinder@gmail.com>
2016-03-01 14:22:55 +0100

    DA1458x Beacon Demo

---
+++ beacons/app-eddystone.js
@@ -0,0 +1,182 @@
+// JavaScript code for the Dialog Eddystone example app.
+
+// Application object
+var eddystone = {};
+
+(function(){
+
+// Dictionary of beacons.
+var beacons = {};
+
+// Timer that displays list of beacons.
+var timer = null;
+
+// Regions that define which page to show for each beacon.
+var beaconData = {
+	'80:EA:CA:00:00:03': 'page-eddystone-elephant',
+	'81:DD:CA:00:00:03': 'page-eddystone-giraffe'
+};
+
+// Currently displayed page.
+var currentPage = 'page-eddystone-default';
+
+// Check for beacons with an rssi higher than this value
+var rssiLimit = -60; 
+
+eddystone.initialize = function()
+{
+	document.addEventListener(
+		'deviceready',
+		function() { evothings.scriptsLoaded(onDeviceReady) },
+		false);
+	
+	// Display default page
+	gotoPage(currentPage);
+};
+
+// Called as soon as we are ready
+function onDeviceReady()
+{
+	// Start tracking beacons!
+	setTimeout(startScan, 2000);
+	
+	// Timer that refreshes the display.
+	timer = setInterval(updateBeaconList, 2000);
+};
+
+function startScan()
+{
+	console.log('Scan in progress.');
+	evothings.eddystone.startScan(
+		function(beacon)
+		{
+			// Update beacon data.
+			beacon.timeStamp = Date.now();
+			beacons[beacon.address] = beacon;
+		},
+		function(error)
+		{
+			console.log('Eddystone scan error: ' + error);
+		});
+};
+
+// Map the RSSI value to a value between 1 and 100.
+function mapBeaconRSSI(rssi)
+{
+	if (rssi >= 0) return 1; // Unknown RSSI maps to 1.
+	if (rssi < -100) return 100; // Max RSSI
+	return 100 + rssi;
+};
+
+function getSortedBeaconList(beacons)
+{
+	var beaconList = [];
+	for (var key in beacons)
+	{	
+		// Only store beacons we know
+		for (var address in beaconData)
+		{
+			if(beacons[address])
+			{
+				beaconList.push(beacons[key]);
+			}
+		}
+	}
+
+	beaconList.sort(function(beacon1, beacon2)
+	{
+		return mapBeaconRSSI(beacon1.rssi) < mapBeaconRSSI(beacon2.rssi);
+	});
+	return beaconList;
+};
+
+function updateBeaconList()
+{
+	removeOldBeacons();
+	displayBeacon();
+};
+
+function removeOldBeacons()
+{
+	var timeNow = Date.now();
+	for (var key in beacons)
+	{
+		// Only show beacons updated during the last 60 seconds.
+		var beacon = beacons[key];
+		if (beacon.timeStamp - 60000 > timeNow)
+		{
+			delete beacons[key];
+		}
+	}
+};
+
+function displayBeacon()
+{
+	var sortedList = getSortedBeaconList(beacons);
+
+	if(sortedList.length > 0)
+	{
+		var beacon = sortedList[0]; // We only care about the closest one
+		// console.log('Eddystone: ' + beacon.address + ' RSSI: ' + beacon.rssi);
+		var pageId = beaconData[beacon.address];
+
+		// If the beacon is close and represents a new page, then show the page.
+		if(beacon.rssi >= rssiLimit && currentPage != pageId)
+		{
+			gotoPage(pageId);
+			return;
+		}
+
+		// If the beacon represents the current page but is far away,
+		// then show the default page.
+		if (beacon.rssi < rssiLimit && currentPage == pageId)
+		{
+			gotoPage('page-eddystone-default');
+			return;
+		}
+	}
+	else
+	{
+		// No beacons found, show default page
+		gotoPage('page-eddystone-default');
+		return;
+	}
+};
+
+function uint8ArrayToString(uint8Array)
+{
+	function format(x)
+	{
+		var hex = x.toString(16);
+		return hex.length < 2 ? '0' + hex : hex;
+	}
+
+	var result = '';
+	for (var i = 0; i < uint8Array.length; ++i)
+	{
+		result += format(uint8Array[i]) + ' ';
+	}
+	return result;
+};
+
+function gotoPage(pageId)
+{
+	hidePage(currentPage);
+	showPage(pageId);
+	currentPage = pageId;
+};
+
+function showPage(pageId)
+{
+	document.getElementById(pageId).style.display = 'block';
+};
+
+function hidePage(pageId)
+{
+	document.getElementById(pageId).style.display = 'none';
+};
+
+})(); // End of closure.
+
+// Initialize eddystone
+eddystone.initialize();
---
+++ beacons/app-ibeacon.js
@@ -0,0 +1,152 @@
+// JavaScript code for the Dialog iBeacon example app.
+
+// Application object.
+var ibeacon = {};
+
+(function(){
+
+// Regions that define which page to show for each beacon.
+var beaconRegions =
+[
+	{
+		id: 'page-ibeacon-elephant',
+		uuid:'585CDE93-1B01-42CC-9A13-25009BEDC65E',
+		major: 1,
+		minor: 1
+	},
+	{
+		id: 'page-ibeacon-giraffe',
+		uuid:'595CDE93-1B01-42CC-9A13-25009BEDC65E', // Notice the difference in UUID
+		major: 1,
+		minor: 1
+	}
+];
+
+// Currently displayed page.
+var currentPage = 'page-ibeacon-default';
+
+// Check for beacons with an rssi higher than this value
+var rssiLimit = -60; 
+
+ibeacon.initialize = function()
+{
+	document.addEventListener(
+		'deviceready',
+		function() { evothings.scriptsLoaded(onDeviceReady) },
+		false);
+	
+	// Display default page
+	gotoPage(currentPage);
+}
+
+// Called when Cordova are plugins initialised,
+// the iBeacon API is now available.
+function onDeviceReady()
+{
+	// Specify a shortcut for the location manager that
+	// has the iBeacon functions.
+	window.locationManager = cordova.plugins.locationManager;
+
+	// Start tracking beacons!
+	startScanForBeacons();
+}
+
+function startScanForBeacons()
+{
+	//console.log('startScanForBeacons')
+
+	// The delegate object contains iBeacon callback functions.
+	var delegate = new cordova.plugins.locationManager.Delegate()
+
+	delegate.didDetermineStateForRegion = function(pluginResult)
+	{
+		//console.log('didDetermineStateForRegion: ' + JSON.stringify(pluginResult))
+	}
+
+	delegate.didStartMonitoringForRegion = function(pluginResult)
+	{
+		//console.log('didStartMonitoringForRegion:' + JSON.stringify(pluginResult))
+	}
+
+	delegate.didRangeBeaconsInRegion = function(pluginResult)
+	{
+		//console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult))
+		didRangeBeaconsInRegion(pluginResult);
+	}
+
+	// Set the delegate object to use.
+	locationManager.setDelegate(delegate);
+
+	// Start monitoring and ranging our beacons.
+	for (var r in beaconRegions)
+	{
+		var region = beaconRegions[r];
+
+		var beaconRegion = new locationManager.BeaconRegion(
+			region.id, region.uuid, region.major, region.minor);
+
+		// Start monitoring.
+		locationManager.startMonitoringForRegion(beaconRegion)
+			.fail(console.error)
+			.done();
+
+		// Start ranging.
+		locationManager.startRangingBeaconsInRegion(beaconRegion)
+			.fail(console.error)
+			.done();
+	}
+}
+
+// Display pages depending of which beacon is close.
+function didRangeBeaconsInRegion(pluginResult)
+{
+	// There must be a beacon within range.
+	if (0 == pluginResult.beacons.length)
+	{
+		return;
+	}
+
+	// Our regions are defined so that there is one beacon per region.
+	// Get the first (and only) beacon in range in the region.
+	var beacon = pluginResult.beacons[0];
+
+	// The region identifier is the page id.
+	var pageId = pluginResult.region.identifier;
+
+	// If the beacon is close and represents a new page, then show the page.
+	if (beacon.rssi >= rssiLimit && currentPage != pageId)
+	{
+		gotoPage(pageId);
+		return;
+	}
+
+	// If the beacon represents the current page but is far away,
+	// then show the default page.
+	if (beacon.rssi < rssiLimit && currentPage == pageId)
+	{
+		gotoPage('page-ibeacon-default');
+		return;
+	}
+}
+
+function gotoPage(pageId)
+{
+	hidePage(currentPage);
+	showPage(pageId);
+	currentPage = pageId;
+}
+
+function showPage(pageId)
+{
+	document.getElementById(pageId).style.display = 'block';
+}
+
+function hidePage(pageId)
+{
+	document.getElementById(pageId).style.display = 'none';
+}
+
+})(); // End of closure
+
+// Initialize iBeacon
+ibeacon.initialize()
